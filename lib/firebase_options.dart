357803113280644bf2b2ed8a845ac18013547333
SaulSandovalM <sauldevelop@gmail.com>
2025-05-30 13:01:51 -0600

    dotenv

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,7 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -54,12 +54,12 @@ class DefaultFirebaseOptions {
   }
 
   static final FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyAB6P86pR1Vb-5ikLXd2ppX3c_0ryUVuSE',
-    appId: '1:302396788105:web:8795db8fb9ebb99cc96558',
-    messagingSenderId: '302396788105',
-    projectId: 'gestec-42dff',
-    authDomain: 'gestec-42dff.firebaseapp.com',
-    storageBucket: 'gestec-42dff.firebasestorage.app',
-    measurementId: 'G-0K0NDX1S8J',
+    apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
+    appId: dotenv.env['FIREBASE_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? '',
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+    measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID'] ?? '',
   );
 }
